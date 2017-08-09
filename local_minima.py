c9e836c5fddb4287d2de3968699e1a26b5216ba4
fangxiaowen <164426683@qq.com>
2017-08-09 13:40:16 -0400

    find local minimas and saddles

--- local_minima.py
+++ local_minima.py
@@ -1,34 +1,73 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 11 15:27:01 2017
-
-@author: Xiaowen Fang
-"""
 import scipy
-from basinfinder import alpha_shape, Basin_Finder
+from basinfinder import alpha_shape
 from descartes import PolygonPatch
 import numpy as np
 import shapely.geometry as geometry
 #from shapely.ops import cascaded_union, polygonize
-import math
 from kernelregression import KernelRegression     #This is the kd_tree version written by Xiaowen Fang
-#import rpy2.robjects as robjects
 import matplotlib.pyplot as plt
 from matplotlib  import cm
 import matplotlib as mpl
-import pickle
 from time import clock
-from multiprocessing import Process, Manager, Array, Pool, Lock
-import multiprocessing
-import os
-import time, threading
 import sys
+from sklearn.preprocessing import normalize
+from sklearn.metrics.pairwise import pairwise_distances
+import numdifftools as nd
+import math
+import pickle
+import time
+
+
+def find_saddle(x):
+    """
+    start from local extrema and find saddle point
+    Parameters:
+        - x : 2D point
+    Returns:
+        2D saddle point
+    """
+    
+    sl = 0.1    #step length
+    i=0
+    try:
+        while not ((abs(G(x)[0]) < 0.2) and (abs(G(x)[1]) < 0.2) and (np.linalg.eigvals(H(x)).prod() < 0)):     #stop critiaria. Saddle point condition. egenvalue1 * egenvalue2 < 0
+        #for j in range(310): 
+            i += 1
+            if i > 100 or x[0] < -15 or x[0] > 20 or x[1] < -20 or x[1] > 15:
+                return
+            orthon = H(x)[0]    #orthonal to the searching direction    
+            tagent = np.array([orthon[1], -orthon[0]])    #searching direction
+                
+            x_new = x + sl * normalize(tagent.reshape(1,-1))[0]
+            #print("Gradient of new pont is : ",G(x_new))
+            #print("\nEgienvalue isssssssssssssssssss : ",np.linalg.eigvals(H(x_new)))
+            #time.sleep(1)
+            trace.append(x_new)
+            
+            while not (abs(G(x_new)[0]) < 0.5):
+                direc_derive = np.dot(H(x_new)[0], normalize(orthon.reshape(1,-1))[0])
+                print('direc_derive is : ', direc_derive)
+                #step length is soo tiny!
+                x_new = x_new - 0.001 * direc_derive * normalize(orthon.reshape(1,-1))[0]
+                print('X_new is : ', x_new)
+                corrector.append(x_new)   
+                print('Gradient of x_new is : ', G(x_new))
+            print('corrector step is finished!\n')
+            x = x_new
+    except ValueError as e:
+        print('Gradient and Hessian are : ', G(x),'\n', H(x))
+        print('X_new is : ', x_new)
+        print("x is : ", x)
+        return
+        #print("G(x) is : ", G(x), " H(x) is : ", H(x))
+    
+    return x
+
+#trace = [x for x in trace if ((abs(G(x)[0] - 0) < 0.15) and (abs(G(x)[1]) < 0.1) and (np.linalg.eigvals(H(x)).prod() < 0))]
 
 if __name__ == '__main__':
-    #multiprocessing.freeze_support()
-    print('oaky here?')
-    #manager = Manager()
-    print('still okay here? Manager Object is created successfully!')
+    #print('still okay here? Manager Object is created successfully!')
     
     #load data from the .txt file
     protein_data = np.dtype([('col1', 'S20'), ('col2', 'S20'),('col3', 'S20'), ('energy', 'f4'), ('col5', 'f4'), ('col6', 'f4'),('col7', 'f4'),('col8', 'f4'),('col9', 'f4'),('col10', 'f4'),('col11', 'f4'),('col12', 'f4'),('col13', 'f4'),('col14', 'f4'),('col15', 'f4')])
@@ -39,10 +78,13 @@ if __name__ == '__main__':
     e_loadData = clock()    #timing
     print('data loaded')
     #Draw the alpha shape from all data points
-    concave_hull, edge_points = alpha_shape(np.delete(points, 2, 1), alpha=0.6)    #concave_hull contains all the polygons (all the boundary)
+    #concave_hull, edge_points = alpha_shape(np.delete(points, 2, 1), alpha=0.6)    #concave_hull contains all the polygons (all the boundary)
+    concave_hull = pickle.load(open(r'C:\Users\Administrator\Desktop\concave_hull.pickle', 'rb'))
     print('alpha shape builded')    
-    #define grid points, [pc1, pc2]
+    #alpha_shape = open(r'C:\Users\Administrator\Desktop\concave_hull.pickle', 'wb')
+    #pickle.dump(concave_hull,alpha_shape)    
     
+    #define grid points, [pc1, pc2]    
     gsz = 1 #Grid size. Could be user input.
     xgridlow, ygridlow, xgridhigh, ygridhigh = concave_hull.bounds
     xgrids =    np.arange(xgridlow - gsz, xgridhigh, gsz)
@@ -64,9 +106,74 @@ if __name__ == '__main__':
 
     f, fprime = kr_model.predict_single, kr_model.fprime
     
+    H = nd.Hessian(f)   #Hessian 
+    G = nd.Gradient(f)  #Gradient
+    print(H)
+    #sys.exit(0)
+    
+    
+    
+    """
     s_newton = clock()
+    local_para, local_minima = [], []
+    
     for i in np.delete(native_grids, 2, 1):
             x = scipy.optimize.fmin_ncg(f, np.array(i), fprime)
+            local_para.append(x)
+            #local_minima.append(x[1])
             print(x)
     f_newton = clock()
-    print('time is : ', f_newton - s_newton)
+    print('time to find local minima is : ', f_newton - s_newton)
+    """
+    
+    #local = open(r'C:\Users\Administrator\Desktop\local_para.pickle', 'wb')
+    #pickle.dump(local_para,local)    
+    local_para = pickle.load(open(r'C:\Users\Administrator\Desktop\local_para.pickle', 'rb'))
+    #local_para=local_para[~np.triu((pairwise_distances(local_para) < 0.1),1).any(0)]  #remove duplicates
+    
+    li = []
+    trace, corrector = [], []
+    for i in local_para:
+        if not math.isnan(i[0]) and abs(i[0]) < 25 and abs(i[1]) < 25:
+            li.append(find_saddle(np.array(i)))
+    #li = pickle.load(open(r'C:\Users\Administrator\Desktop\saddles.pickle', 'rb'))     #list of saddle points
+    
+    all_trace = []
+    all_trace.append(trace)
+    sys.exit(0)
+    #saddle = open(r'C:\Users\Administrator\Desktop\saddles.pickle', 'wb')
+    #pickle.dump(li,saddle)
+    #tr = open(r'C:\Users\Administrator\Desktop\trace1_0.4_direction2.pickle', 'wb')
+    #pickle.dump(trace,tr)
+    
+    fig = pickle.load(open(r'C:\Users\Administrator\Desktop\trace1_color.pickle', 'rb'))
+    #trace = pickle.load(open(r'C:\Users\Administrator\Desktop\trace1_0.4.pickle', 'rb'))
+    
+    
+    
+    norm = mpl.colors.Normalize(vmin=-6650, vmax=-6410)
+    cmap = cm.jet
+    m = cm.ScalarMappable(norm=norm, cmap=cmap)
+
+    #This is the figure we want
+    
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111)
+    margin = .3
+    x_min, y_min, x_max, y_max = concave_hull.bounds
+    ax.set_xlim([x_min-margin, x_max+margin])
+    ax.set_ylim([y_min-margin, y_max+margin])
+    patch = PolygonPatch(concave_hull, fc=m.to_rgba(-6270), ec='#000000', fill=True, zorder=-1)
+    #local_para = np.array(local_para)
+    ax.add_patch(patch)
+    
+
+    ax = fig.add_subplot(111)
+    surf = ax.scatter(np.array(local_para)[:,0], np.array(local_para)[:,1], color = 'yellow')
+
+    surf2 = ax.scatter(np.array(trace)[:,0], np.array(trace)[:,1], s=0.5, color='blue')
+    surf3 = ax.scatter(np.array(li)[:,0], np.array(li)[:,1], color='black')
+    ge = open(r'C:\Users\Administrator\Desktop\saddle_points.pickle', 'wb')
+    pickle.dump(fig,ge)    
+    #fig.colorbar(surf, shrink=0.5, aspect=5)
+    fig.savefig(r'C:\Users\Administrator\Desktop\saddle_points' + kernel + '_.png')
